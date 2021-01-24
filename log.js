c50f10062b9a4b3bcfff1de5eecfc70bc6c0cccd
Tushar Kumar <73745126+Tushar-Official@users.noreply.github.com>
2021-01-24 11:57:34 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,11 @@
+function add(){
+    let addtext=document.getElementById("text").value;
+    let addpass=document.getElementById("pass").value;
+
+    if(addtext==""&& addpass==""){
+        alert("Enter the details");
+    }
+    else{
+        alert("You are Logged in");
+    }
+}
